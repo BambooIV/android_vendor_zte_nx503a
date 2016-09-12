c949032755d1cd6148d9b3068a6deec5f8ed906f
BambooIV <ningdisheng@hotmail.com>
2016-09-12 22:29:32 +0800

    nx503a: Refresh vendor blobs after proprietary files list change

--- oem.mk
+++
@@ -1,3 +0,0 @@
-PRODUCT_COPY_FILES += \
-    vendor/zte/nx503a/oem/nubia_IRControl.apk:system/app/nubia_IRControl/nubia_IRControl.apk \
-    vendor/zte/nx503a/oem/nubia_IRControl_uei_sdk.apk:system/app/nubia_IRControl_uei_sdk/nubia_IRControl_uei_sdk.apk
